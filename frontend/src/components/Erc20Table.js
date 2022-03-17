86e8f91a5ad7f135055e703d780237fb896791da
square <artubr@gmail.com>
2022-03-18 00:33:05 +0100

    Add ERC20 Table

---
+++ frontend/src/components/Erc20Table.js
@@ -0,0 +1,146 @@
+import React, { useEffect, useState } from "react";
+import { useTable, usePagination, useSortBy } from "react-table";
+
+export default function Erc20Table() {
+  const API_SERVER = process.env.REACT_APP_API_URL;
+
+  const columns = React.useMemo(
+    () => [
+      {
+        Header: "Name",
+        accessor: "name", // accessor is the "key" in the data
+      },
+      {
+        Header: "Symbol",
+        accessor: "symbol",
+      },
+      {
+        Header: "Amount",
+        accessor: "amount",
+      },
+      {
+        Header: "Tx Number",
+        accessor: "txnumber",
+      },
+    ],
+
+    []
+  );
+  const [data, setData] = useState([
+    {
+      name: "Loading...",
+      symbol: "Loading...",
+      amount: "Loading...",
+      txnumber: "Loading...",
+    },
+  ]);
+  useEffect(() => {
+    const requestOptions = {
+      method: "GET",
+    };
+    fetch(`${API_SERVER}/erc20/table`, requestOptions)
+      .then((response) => response.json())
+      .then((data) => {
+        setData(data);
+      });
+  }, [API_SERVER]);
+
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    prepareRow,
+    page,
+    canPreviousPage,
+    canNextPage,
+    pageOptions,
+    pageCount,
+    gotoPage,
+    nextPage,
+    previousPage,
+    setPageSize,
+    state: { pageIndex, pageSize },
+  } = useTable(
+    {
+      columns,
+      data,
+      initialState: { pageIndex: 0, sortBy: [{ id: "txnumber", desc: true }] },
+    },
+    useSortBy,
+    usePagination
+  );
+
+  return (
+    <>
+      <table {...getTableProps()} className="table">
+        <thead>
+          {headerGroups.map((headerGroup) => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map((column) => (
+                <th {...column.getHeaderProps()}>{column.render("Header")}</th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {page.map((row, i) => {
+            prepareRow(row);
+            return (
+              <tr {...row.getRowProps()}>
+                {row.cells.map((cell) => {
+                  return (
+                    <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
+                  );
+                })}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+      <div className="pagination">
+        <button onClick={() => gotoPage(0)} disabled={!canPreviousPage}>
+          {"<<"}
+        </button>{" "}
+        <button onClick={() => previousPage()} disabled={!canPreviousPage}>
+          {"<"}
+        </button>{" "}
+        <button onClick={() => nextPage()} disabled={!canNextPage}>
+          {">"}
+        </button>{" "}
+        <button onClick={() => gotoPage(pageCount - 1)} disabled={!canNextPage}>
+          {">>"}
+        </button>{" "}
+        <span>
+          Page{" "}
+          <strong>
+            {pageIndex + 1} of {pageOptions.length}
+          </strong>{" "}
+        </span>
+        <span>
+          | Go to page:{" "}
+          <input
+            type="number"
+            defaultValue={pageIndex + 1}
+            onChange={(e) => {
+              const page = e.target.value ? Number(e.target.value) - 1 : 0;
+              gotoPage(page);
+            }}
+            style={{ width: "100px" }}
+          />
+        </span>{" "}
+        <select
+          value={pageSize}
+          onChange={(e) => {
+            setPageSize(Number(e.target.value));
+          }}
+        >
+          {[10, 20, 30, 40, 50].map((pageSize) => (
+            <option key={pageSize} value={pageSize}>
+              Show {pageSize}
+            </option>
+          ))}
+        </select>
+      </div>
+    </>
+  );
+}
